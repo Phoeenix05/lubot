49e6e0b8a9dd240ed4ff6896fb0bc0ef21476675
Phoeenix05 <79942428+Phoeenix05@users.noreply.github.com>
2022-06-09 19:14:10 +0300

    Updated commands

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
 import os, dotenv, discord
+from discord.ext import commands
 
 # Setup
 os.system('clear')   # Clear terminal window
 dotenv.load_dotenv() # Load .env
 
 TOKEN = os.getenv('TOKEN') # Get token from enviroment variables
-client = discord.Client()  # Create discord bot client
-channel: discord.TextChannel = None
-
-def get_channel() -> None:
-  global channel
-  channel = client.get_channel(953569962486816791)
-
-def startswith_lower(msg: str, prefix: str) -> bool:
-  return msg.lower().startswith(prefix)
+client = commands.Bot(command_prefix=('/', '!'))  # Create discord bot client
 
 @client.event
 async def on_ready():
-  get_channel() # Get general channel
-  await channel.send('Lubot connected!')
+  # get_channel() # Get general channel
+  # await channel.send('Lubot connected!')
   print(f'[Connected] {client.user}')
 
-@client.event
-async def on_message(message: discord.Message):
-  if message.author.id == client.user.id: return
-  
-  if startswith_lower(message.content, '!ping'):
-    await message.reply('pong')
-  
-  if startswith_lower(message.content, '/stop'):
-    # Send message to channel
-    await channel.send(f'Shutting down {client.user.name}')
-    await client.close() # Close bot client
+@client.command()
+async def ping(ctx: commands.Context):
+  if (ctx.prefix != '!'): return
+  await ctx.reply('pong', mention_author=False)
+
+@client.command()
+async def stop(ctx: commands.Context):
+  await ctx.send(f'Shutting down {client.user.name}')
+  await client.close()
 
 client.run(TOKEN) # Run bot

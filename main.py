0a129084d7771f4ddeea96a86eb7ba2bace46683
Phoeenix05 <79942428+Phoeenix05@users.noreply.github.com>
2022-06-10 18:54:15 +0300

    Restart command

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os, dotenv, nextcord
+import os, sys, dotenv, nextcord
 from nextcord import Interaction
 from nextcord.ext import commands
+from time import time
 
 intents = nextcord.Intents.default()
 intents.members = True
@@ -15,17 +16,20 @@ async def on_ready():
 
 @client.slash_command('ping')
 async def ping(interaction: Interaction):
-  await interaction.response.send_message('pong')
+  await interaction.response.send_message(f'{client.latency  * 1000:.0f} ms')
 
 @client.slash_command('stop', 'Stops Lubot')
 async def stop(interaction: Interaction):
   await interaction.response.send_message('Lubot stopped!')
   await client.close()
 
+@client.slash_command('restart', 'Restart Lubot')
+async def restart(interaction: Interaction):
+  await interaction.response.send_message('Restarting!')
+  os.execv(sys.executable, ['python3'] + sys.argv)
 
 # Setup
 if __name__ == '__main__':
-  os.system('clear')   # Clear terminal window
   dotenv.load_dotenv() # Load .env
   
   TOKEN = os.getenv('TOKEN') # Get token from enviroment variables
